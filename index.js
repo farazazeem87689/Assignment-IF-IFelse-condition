f0ddcc7748854b2db70cd5239a6be7b9b36adba5
Faraz Azeem <farazazeem69@gmail.com>
2023-09-01 08:44:01 -0700

    If -IfElse-condition

---
+++ index.js
@@ -0,0 +1,56 @@
+"use strict";
+// write a program that calculate the area of a rectangle?
+var area = 10;
+var width = 29;
+var result = area * width;
+console.log("Area of Rectangular = " + result);
+// Write a program that take input and calculate the volume of cube?
+var a = 3;
+var b = 4;
+var c = 2;
+var result = a * b * c;
+console.log("Volume of Cube = " + result);
+// write a program that checks if a given numbver is positive negative and zero?
+var num1 = 12;
+var num2 = 4;
+if (num1 > 0) {
+    console.log("Number is positive");
+}
+else if (num2 < 0) {
+    console.log("Number is negative");
+}
+else {
+    console.log("number is zero");
+}
+// Write a program that check if a given number is even or odd?
+var a = 24;
+if (a % 2 == 0) {
+    console.log("numer is even");
+}
+else {
+    console.log("number is odd");
+}
+// write a program that determinate the if a person is eligible to vote based on their age?
+var age = 20;
+if (age >= 20) {
+    console.log("vote is Eligible");
+}
+else {
+    console.log("vote is not Eligible");
+}
+//Write a program that calcultes the result of the mathematical the result of ?
+var result = ((10 + 5) * 3 - 2) / (4 % 3) - 7;
+(15 * 3 - 2) / (4 % 3) - 7;
+43 / 1 - 7;
+43 - 7;
+36;
+console.log(result);
+var section = 'A';
+switch (section) {
+    case "a":
+        console.log("your section is a");
+        break;
+    default:
+        console.log("your section is not a ");
+        break;
+}
